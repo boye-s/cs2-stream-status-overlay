1025982a08b541a25c2d87fcc6878805340c104d
Boye Svensson <boyevnsvensson@gmail.com>
2024-10-26 23:08:34 +0200

    Add server app and json file for state

---
+++ server/app.mjs
@@ -0,0 +1,31 @@
+// eslint-disable-next-line no-undef
+import express from "express"
+import { readFileSync, writeFileSync } from "fs"
+const app = express()
+const port = 3000
+
+app.get("/state/get", async (req, res) => {
+    try {
+        const result = await readFileSync(`overlay-state.json`, "utf8")
+        res.send(JSON.parse(result))
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Error reading state")
+    }
+})
+
+app.post("/state/post", async (req, res) => {
+    try {
+        await writeFileSync(`overlay-state.json`, JSON.stringify(req.body, null, 2))
+
+        const result = await readFileSync(`overlay-state.json`, "utf8")
+        res.send(JSON.parse(result))
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Error saving state")
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Overlay state server listening on port ${port}`)
+})
